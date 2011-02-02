2b2f6bda64118124c770910453d94497dd0896cd
Jordan Byron <jordan@ducksoupsoftware.com>
2011-02-02 15:05:29 -0500

    Add basic submission ui

--- app/controllers/puzzles/submissions_controller.rb
+++ app/controllers/puzzles/submissions_controller.rb
@@ -1,2 +1,25 @@
 class Puzzles::SubmissionsController < ApplicationController
+  before_filter :find_puzzle
+
+  def new
+    @submission = @puzzle.submissions.new
+  end
+  
+  def create
+    @submission = @puzzle.submissions.new(params[:submission])
+    
+    @submission.user = current_user
+    
+    if @submission.save
+      redirect_to user_submission_path(current_user, @submission)
+    else      
+      render :action => :new
+    end
+  end
+  
+  private
+  
+  def find_puzzle
+    @puzzle = Puzzle.find(params[:puzzle_id])
+  end
 end
--- app/controllers/users/submissions_controller.rb
+++ app/controllers/users/submissions_controller.rb
@@ -1,2 +1,6 @@
 class Users::SubmissionsController < ApplicationController
+  
+  def show
+    @submission = Submission.find(params[:id])
+  end
 end
