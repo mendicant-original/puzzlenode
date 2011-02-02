2ce009b47f00824bfeb0f177efaace762ad7c4f7
Jordan Byron <jordan@ducksoupsoftware.com>
2011-02-02 11:50:16 -0500

    Add github authentication

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -4,11 +4,11 @@ class Authorization < ActiveRecord::Base
   validates_uniqueness_of :uid, :scope => :provider
   
   def self.find_from_hash(hash)
-    find_by_provider_and_uid(hash['provider'], hash['uid'])
+    find_by_provider_and_uid(hash['provider'], hash['uid'].to_s)
   end
 
   def self.create_from_hash(hash, user = nil)
     user ||= User.create_from_hash!(hash)
-    Authorization.create(:user => user, :uid => hash['uid'], :provider => hash['provider'])
+    Authorization.create(:user => user, :uid => hash['uid'].to_s, :provider => hash['provider'])
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,5 @@ keys = YAML.load_file("#{Rails.root}/config/omniauth.yml")
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, keys['twitter']['key'], keys["twitter"]['secret']
+  provider :github, keys['github']['client'], keys['github']['secret']
 end
