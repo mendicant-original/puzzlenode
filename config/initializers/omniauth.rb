24115bc7f644974c4ca216e36ba2dc9343a157a5
Jordan Byron <jordan@ducksoupsoftware.com>
2011-02-02 10:08:40 -0500

    Add twitter auth and bare sessions controller

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+keys = YAML.load_file("#{Rails.root}/config/omniauth.yml")
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, keys['twitter']['key'], keys["twitter"]['secret']
+end
