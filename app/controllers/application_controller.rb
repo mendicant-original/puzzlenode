89b81422f4b5904ce7251643bcfe92883bad2f67
Jordan Byron <jordan@ducksoupsoftware.com>
2011-02-02 10:33:12 -0500

    Add basic auth functions to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  protected
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  helper_method :current_user, :signed_in?
+
+  def current_user=(user)
+    @current_user = user
+    session[:user_id] = user.id
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_many :authorizations
+  
+  def self.create_from_hash!(hash)
+    create(:name => hash['user_info']['name'])
+  end
 end
