82e9458a8baee0612e30ef22314a280fc154f461
Jordan Byron <jordan@ducksoupsoftware.com>
2011-02-02 10:59:11 -0500

    Add home controller for root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Puzzlenode::Application.routes.draw do
+  root :to => 'home#index'
+
   match '/auth/:provider/callback', :to => 'sessions#create'
   match '/logout', :to => 'sessions#destroy'
   match '/login',  :to => 'sessions#new'
