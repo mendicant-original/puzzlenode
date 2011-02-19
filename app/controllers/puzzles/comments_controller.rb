1e6b6d6bd0c6f62a9bb050da38ecf810fb5d8d36
Jordan Byron <jordan@ducksoupsoftware.com>
2011-02-19 15:30:32 -0500

    Cheaters never prosper

--- app/controllers/puzzles/comments_controller.rb
+++ app/controllers/puzzles/comments_controller.rb
@@ -1,7 +1,16 @@
 class Puzzles::CommentsController < Puzzles::Base
-
+  before_filter :answered_correct_only
+  
   def index
     
   end
 
+  private
+  
+  def answered_correct_only
+    unless @puzzle.answered_correctly?(current_user)
+      flash[:error] = "You must correctly answer this puzzle before you can access comments"
+      redirect_to puzzle_path(@puzzle)
+    end
+  end
 end
