c2fe866e306fe5ade7198acea838003609868ccb
Jordan Byron <jordan@ducksoupsoftware.com>
2011-02-19 15:34:34 -0500

    Menu tweaks

--- app/controllers/puzzles/comments_controller.rb
+++ app/controllers/puzzles/comments_controller.rb
@@ -9,7 +9,7 @@ class Puzzles::CommentsController < Puzzles::Base
   
   def answered_correct_only
     unless @puzzle.answered_correctly?(current_user)
-      flash[:error] = "You must correctly answer this puzzle before you can access comments"
+      flash[:error] = "You must answer this puzzle correctly before you can access comments"
       redirect_to puzzle_path(@puzzle)
     end
   end
