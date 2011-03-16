e4304a6b41734666657ebb51ea483c19b2ad8c65
Jordan Byron <jordan@ducksoupsoftware.com>
2011-03-16 12:52:23 -0400

    Add style to announcements

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,6 +1,13 @@
 class AnnouncementsController < ApplicationController
   def index
     @announcements = Announcement.order("created_at DESC")
+    
+    respond_to do |format|
+      format.html do
+        @announcements = @announcements.paginate(:page => params[:page], :per_page => 10)
+      end
+      format.rss { render :layout => false }
+    end
   end
   
   def show
