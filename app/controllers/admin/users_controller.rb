b71ce256718d6e8d1253b6b0279324d853e0db8f
Jordan Byron <jordan@ducksoupsoftware.com>
2011-03-18 14:00:07 -0400

    Sort users by correct solutions then attempted solutions

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,11 @@
 class Admin::UsersController < Admin::Base
   
   def index
-    @users = User.order("created_at").paginate(:page => params[:page])
+    order_by = []
+    order_by << "(SELECT COUNT(*) FROM submissions WHERE correct IS TRUE AND user_id IN (users.id)) DESC"
+    order_by << "(SELECT COUNT(*) FROM submissions WHERE user_id IN (users.id)) DESC"
+    
+    @users = User.order(order_by.join(",")).paginate(:page => params[:page])
   end
   
 end
