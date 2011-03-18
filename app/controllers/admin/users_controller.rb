45a9b71dcaf65cd98cb3afc6cd9d9500877f76ab
Jordan Byron <jordan@ducksoupsoftware.com>
2011-03-18 14:08:08 -0400

    Sort by created_at in users admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,6 +4,7 @@ class Admin::UsersController < Admin::Base
     order_by = []
     order_by << "(SELECT COUNT(*) FROM submissions WHERE correct IS TRUE AND user_id IN (users.id)) DESC"
     order_by << "(SELECT COUNT(*) FROM submissions WHERE user_id IN (users.id)) DESC"
+    order_by << "users.created_at"
     
     @users = User.order(order_by.join(",")).paginate(:page => params[:page])
   end
