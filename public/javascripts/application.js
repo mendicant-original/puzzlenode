ec5499096f774e927aab0dc3c2d5c7a1dfb15861
Jordan Byron <jordan@ducksoupsoftware.com>
2011-03-22 16:49:32 -0400

    Add about section

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,3 +5,19 @@ $(function(){
     window.location = button.attr('data-url');
   });
 });
+
+var PuzzleNode = {};
+
+PuzzleNode.about = function(){
+  var visible = $("#about").is(":visible");
+  
+  $("#footer").toggleClass("active"); 
+  
+  if(visible) $.scrollTo('header', 500, { easing:'swing' });
+ 
+  $('#footer .teaser').fadeToggle();
+  
+  $('#about').slideToggle(200, function(){
+    if(!visible) $.scrollTo('#about', 500, { easing:'swing' });
+  });
+};
