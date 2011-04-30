8895f349512411a2cdf7d567540e07100d5afb81
Jordan Byron <jordan@ducksoupsoftware.com>
2011-04-30 18:35:42 -0400

    Give certain users draft access

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < Admin::Base
+  before_filter :find_user, :only => [:edit, :update]
   
   def index
     order_by = []
@@ -7,6 +8,25 @@ class Admin::UsersController < Admin::Base
     order_by << "users.created_at"
     
     @users = User.order(order_by.join(",")).paginate(:page => params[:page])
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "User sucessfully updated."
+      redirect_to admin_users_path
+    else
+      render :action => :edit
+    end
+  end
+  
+  private
+  
+  def find_user
+    @user = User.find(params[:id])
   end
   
 end
