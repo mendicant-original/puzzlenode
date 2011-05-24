c3cee67741c8bf132bec60edb7d5241b1fb0e044
Jordan Byron <jordan@ducksoupsoftware.com>
2011-05-24 11:58:56 -0400

    Now that BohConf is over, time to remove all spot the baddy references

--- app/helpers/users/submissions_helper.rb
+++ app/helpers/users/submissions_helper.rb
@@ -1,12 +1,2 @@
 module Users::SubmissionsHelper
-  
-  # A special treat for BohConf ...
-  #
-  def baddy
-    if @submission.correct
-      link_to "You got 'em. Good job buddy!", "http://spotthebaddy.com", :target => "_blank"
-    else
-      link_to "The Baddy Got Away. Give it another try!", "http://spotthebaddy.com", :target => "_blank"
-    end
-  end
 end
