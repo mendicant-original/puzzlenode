969e885d3d949432baacb92e975acd67cf87e970
Solomon White <rubysolo@gmail.com>
2011-05-24 10:21:54 -0600

    haiku fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ActiveRecord::Base
     # TODO
     # Panda loves Arel
     # but subqueries are so hard!
-    # How to make a happy bear?
+    # please make bear happy.
     User.contestant.find_by_sql(%Q{
       SELECT users.*, solved, attempts, elapsed
         FROM users INNER JOIN (#{ solutions.to_sql }) q1 ON q1.user_id = users.id
