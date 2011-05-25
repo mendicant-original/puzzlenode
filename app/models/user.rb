5a66dd46dd17251324576b1c98ae9eb84fa8a9d5
Solomon White <rubysolo@gmail.com>
2011-05-24 18:03:47 -0600

    remove EXTRACT EPOCH, go with last submitted correct solution

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,18 +29,30 @@ class User < ActiveRecord::Base
   end
 
   def self.leaderboard(limit=10)
-    solutions = Submission.correct.select('user_id, SUM(EXTRACT(EPOCH FROM created_at)) AS elapsed, COUNT(*) AS solved').group('user_id')
+    # For each user, we need a count of correct submissions, along with
+    # the date of the most recent correct submission.
+    # We also need a count of all submissions correct or not.
+    solutions = Submission.correct.select('user_id, MAX(created_at) AS latest_solution, COUNT(*) AS solved').group('user_id')
     attempts  = Submission.select('user_id, COUNT(*) AS attempts').group('user_id')
 
+    # We want to sort results by:
+    #   1) highest number of correct scores
+    #   2) lowest number of attempts
+    #   3) earliest creation date of the *last* correct submission
+    #
+    # This inner join is a bit fugly, but it allows us to gather all
+    # the data and use it both for sorting and for displaying with
+    # one database query.
+    #
     # TODO
     # Panda loves Arel
     # but subqueries are so hard!
     # please make bear happy.
     User.contestant.find_by_sql(%Q{
-      SELECT users.*, solved, attempts, elapsed
+      SELECT users.*, solved, attempts, latest_solution
         FROM users INNER JOIN (#{ solutions.to_sql }) q1 ON q1.user_id = users.id
-                   INNER JOIN (#{ attempts.to_sql }) q2 ON q2.user_id = users.id
-       ORDER BY solved DESC, attempts, elapsed
+                   INNER JOIN (#{ attempts.to_sql  }) q2 ON q2.user_id = users.id
+       ORDER BY solved DESC, attempts, latest_solution
        LIMIT #{ limit }
     })
   end
