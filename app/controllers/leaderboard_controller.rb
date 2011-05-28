0c86007fe2c351171cbeac3fd1d7328cc4c65d9d
Solomon White <rubysolo@gmail.com>
2011-05-28 17:47:23 -0600

    add some styling for leaderboard

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -2,5 +2,10 @@ class LeaderboardController < ApplicationController
   
   def index
     @users = User.leaderboard
+
+    @top_three = []
+    @top_three << [:gold,   @users.shift]
+    @top_three << [:silver, @users.shift]
+    @top_three << [:bronze, @users.shift]
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def gravatar_image(user)
+    key = Digest::MD5.hexdigest(user.email.to_s.downcase)
+    image_tag "http://www.gravatar.com/avatar/#{ key }?d=retro"
+  end
+
+  def github_url(user)
+    "http://github.com/#{ user.nickname }"
+  end
 end
