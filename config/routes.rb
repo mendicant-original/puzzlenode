f198d9386bb738fd99a1b51b010be6d082ac24f1
Solomon White <rubysolo@gmail.com>
2011-05-31 17:29:40 -0600

    add leaderboard link to main nav

--- config/routes.rb
+++ config/routes.rb
@@ -4,23 +4,23 @@ Puzzlenode::Application.routes.draw do
   match '/auth/:provider/callback',      :to => 'sessions#create'
   match '/logout' => 'sessions#destroy', :as => 'logout'
   match '/login' => 'sessions#new',      :as => 'login'
-  
+
   resources :puzzles do
     resources :submissions, :controller => "Puzzles::Submissions"
     resources :comments,    :controller => "Puzzles::Comments"
   end
-  
-  match '/puzzles/:id/attachments/:file', :to => 'puzzles#attachments', 
+
+  match '/puzzles/:id/attachments/:file', :to => 'puzzles#attachments',
                                           :as => "attachment"
-  
+
   resources :users do
     resources :submissions, :controller => "Users::Submissions"
   end
-  
+
   resources :announcements
-  
-  match '/leaderboard', :to => "leaderboard#index"
-  
+
+  match '/leaderboard', :to => "leaderboard#index", :as => 'leaderboard'
+
   namespace :admin do
     resources :puzzles
     resources :users
