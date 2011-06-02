22a287bd557c26586821f2fa61bd09bed40aed1f
Jordan Byron <jordan@ducksoupsoftware.com>
2011-06-02 16:51:51 -0400

    Puzzle integration test green

--- test/integration/admin/puzzles_test.rb
+++ test/integration/admin/puzzles_test.rb
@@ -2,29 +2,24 @@ require 'test_helper'
 
 module Admin
   class PuzzlesTest < ActionDispatch::IntegrationTest
-    
+
     setup do
       @admin_user = Factory(:user, :admin => true)
       @admin_user.authorizations.create(:provider => "github", :uid => "12345")
     end
-    
-    teardown do
-      Capybara.use_default_driver
-    end
-    
+
     test "Admin users can create new puzzles" do
-      pending && return
       sign_user_in
-      
+
       visit new_admin_puzzle_path
-      
+
       fill_in "Name",              :with => "Test Puzzle"
       fill_in "Short description", :with => "Test Puzzle Short Desc"
       fill_in "Description",       :with => "Test Puzzle Desc"
-      
+
       click_button "Create Puzzle"
-      
-      assert_current_path admin_puzzles_path
+
+      assert_current_path admin_puzzle_path(Puzzle.order("created_at DESC").first)
     end
   end
 end
