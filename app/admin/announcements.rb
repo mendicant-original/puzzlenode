d97369cb35ec6c27957d795fd4f69370fceaba6b
Jordan Byron <jordan@ducksoupsoftware.com>
2011-06-02 16:07:30 -0400

    Modify announcement index

--- app/admin/announcements.rb
+++ app/admin/announcements.rb
@@ -1,5 +1,12 @@
 ActiveAdmin.register Announcement, :sort_order => 'created_at_desc' do
 
+  index do
+    column :title do |announcement|
+      link_to announcement.title, edit_admin_announcement_path(announcement)
+    end
+    column :created_at
+  end
+
   form do |f|
     f.inputs do
       f.input :author
