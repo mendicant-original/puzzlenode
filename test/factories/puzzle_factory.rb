d6dde0d167ce6578321f4bf21147d4b6828a351b
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-06-07 23:01:09 +0530

    Validate the presence of :file on Submission.

--- test/factories/puzzle_factory.rb
+++ test/factories/puzzle_factory.rb
@@ -3,4 +3,5 @@ Factory.define :puzzle do |p|
   p.short_description "Test Puzzle"
   p.description       "This is a test puzzle.  It doesn't actually do anything."
   p.file              Tempfile.new("test_tempfile")
+  p.released_on       DateTime.yesterday
 end
