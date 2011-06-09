9ae868ccfaa323e31aa17b31d7ba2d283648418e
Jordan Byron <jordan@ducksoupsoftware.com>
2011-06-09 10:43:02 -0400

    Add failing test for PT #14331895

--- test/unit/submission_test.rb
+++ test/unit/submission_test.rb
@@ -4,4 +4,15 @@ class SubmissionTest < ActiveSupport::TestCase
   test "must be able to detect when submission is correct" do
     pending
   end
+
+  test "no more submissions should be allowed once a correct one is submitted" do
+    user   = Factory(:user)
+    file   = Tempfile.new("test_tempfile")
+    puzzle = Factory(:puzzle, :file => file)
+
+    user.submissions.create(:puzzle => puzzle, :file => file)
+    second_submission = user.submissions.create(:puzzle => puzzle, :file => file)
+
+    assert second_submission.new_record?, "Record saved when it shouldn't"
+  end
 end
