84ea89adf12368ccaac36295746a10e694d88023
Jordan Byron <jordan@ducksoupsoftware.com>
2011-07-29 11:07:06 -0400

    Add contest support

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
 require 'support/integration'
+require 'support/mini_contest'
 require 'test_notifier/runner/minitest'
 
 class ActiveSupport::TestCase
