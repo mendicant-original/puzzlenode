2fac47f9ec6e33df09d62ff03785d3ee6ff7f3e0
Jordan Byron <jordan@ducksoupsoftware.com>
2011-09-13 12:40:26 -0400

    Move include to after bundler require

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,11 @@
 require File.expand_path('../boot', __FILE__)
-include Rake::DSL
 require 'rails/all'
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
+
+include Rake::DSL
 
 module Puzzlenode
   class Application < Rails::Application
