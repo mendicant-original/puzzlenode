c873d0cf0239ca5e0c4f87d02dad390583de1f0d
Jordan Byron <jordan@ducksoupsoftware.com>
2011-09-13 12:43:27 -0400

    Sort by tagged by published date and limit viewable

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -1,18 +1,19 @@
 class PuzzlesController < ApplicationController
-  
+
   def index
     @puzzles = Puzzle.published(current_user).order("created_at").all
   end
 
   def tag
-    @puzzles = Puzzle.tagged_with(params[:tag])
+    @puzzles = Puzzle.published(current_user).order("created_at").
+                tagged_with(params[:tag])
 
     render :index
   end
-  
+
   def show
     @puzzle = Puzzle.find(params[:id])
-    
+
     unless @puzzle.published?
       if current_user.nil? || !current_user.draft_access
         flash[:error] = "Sorry, this puzzle hasn't been published yet."
@@ -20,12 +21,12 @@ class PuzzlesController < ApplicationController
       end
     end
   end
-  
+
   def attachments
-    path = File.join(Rails.root, "public", "attachments", params[:id], 
+    path = File.join(Rails.root, "public", "attachments", params[:id],
                      [params[:file], params[:format]].compact.join("."))
-    
+
     send_data(File.binread(path))
   end
-  
+
 end
