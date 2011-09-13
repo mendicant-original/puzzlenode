748fcfa3d3a5a764c0bf9e3019185c2c60c7695f
Jordan Byron <jordan@ducksoupsoftware.com>
2011-09-13 12:29:47 -0400

    Remove tag list from puzzle page

--- test/integration/puzzle/tags_test.rb
+++ test/integration/puzzle/tags_test.rb
@@ -9,15 +9,6 @@ class Puzzle
       @puzzle = Factory(:puzzle, :name => "Puzzle Name", :tag_list => "Game, Graphics")
     end
 
-    test "tags is displayed" do
-      sign_user_in
-
-      visit puzzle_path(@puzzle)
-
-      assert_link "Game"
-      assert_link "Graphics"
-    end
-
     test "list puzzle by tag" do
       invisible_puzzle = Factory(:puzzle, :name => "Invisible Puzzle", :tag_list => "Math, TDD")
 
