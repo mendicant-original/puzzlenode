2e9f84551619d022cccfdd945a4dda3d76fd519e
Jordan Byron <jordan@ducksoupsoftware.com>
2011-10-11 16:02:34 -0400

    Playing around with a user box

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
-  def gravatar_image(user, size=80)
+  def gravatar_image(user, size=80, options={})
     key = Digest::MD5.hexdigest(user.email.to_s.downcase)
-    image_tag "http://www.gravatar.com/avatar/#{ key }?d=retro&size=#{ size }"
+    image_tag "http://www.gravatar.com/avatar/#{ key }?d=retro&size=#{ size }", options
   end
 
   def github_url(user)
