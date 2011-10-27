448fa4b517a07b3c898fc7aede1403e61ee69be5
Jordan Byron <jordan@ducksoupsoftware.com>
2011-10-27 09:25:53 -0400

    Add user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  before_filter :find_user
+
+  def show
+    unless @user
+      flash[:error] = "Sorry, we can't find that user!"
+      redirect_to root_path
+    else
+      @leaderboard = @user.leaderboard_position(User.count)
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by_nickname(params[:id])
+  end
 end
