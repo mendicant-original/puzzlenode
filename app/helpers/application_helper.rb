44f75dc676bd7dad51783c9b0ba2ba92455fb26a
Jordan Byron <jordan@ducksoupsoftware.com>
2011-10-30 16:45:53 -0400

    Update markdown helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,19 +4,14 @@ module ApplicationHelper
     content_for(:title) { page_title }
   end
 
-  def md(content,  options={})
+  def md(content)
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
       :autolink            => true,
       :space_after_headers => true,
       :no_intra_emphasis   => true,
       :fenced_code_blocks  => true)
 
-    html = markdown.render(content)
-
-    # FIXME Use Redcarpet to strip p tags
-    html = html.gsub(/<\/?p>/, '') if options[:no_p]
-
-    html.html_safe
+    markdown.render(content).html_safe
   end
 
 end
