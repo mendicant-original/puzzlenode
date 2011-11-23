a742daa27d698312b57629269d44e1eab60b9b90
Gregory Parkhurst <six.impossible@gmail.com>
2011-11-23 16:21:46 -0600

    Added admin navbar

---
+++ test/integration/admin/navbar_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+module Admin
+  class NavbarTest < ActionDispatch::IntegrationTest
+
+    setup do
+      @admin_user = Factory(:user, :admin => true)
+      @admin_user.authorizations.create(:provider => "github", :uid => "12345")
+    end
+
+    test "Only admins can see admin navbar" do
+      visit root_path
+
+      assert_no_content "Administration"
+
+      sign_user_in
+
+      assert_content "Administration"
+    end
+  end
+end
