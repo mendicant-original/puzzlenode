4e36b808d6fbc50b7af5c2baa146840e4eb211b0
Jingjing Duan <duanjingjing@gmail.com>
2012-01-02 18:05:24 -0800

    add CommentMailer

---
+++ app/mailers/comment_mailer.rb
@@ -0,0 +1,11 @@
+class CommentMailer < ActionMailer::Base
+  default from: "duanjingjing@gmail.com"
+
+  def welcome_email(user)
+    @user = user
+    @user.name = "Jingjing Duan"
+    @user.login = "jduan"
+    @url = "http://example.com/login"
+    mail(:to => "duanjingjing@gmail.com", :subject => "Welcome to my site")
+  end
+end
