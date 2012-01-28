8146ee4d84c44fabac4c512a7a78c161331b2070
Jingjing Duan <duanjingjing@gmail.com>
2012-01-28 12:48:53 -0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+User.create(:name => 'lqq', :nickname => 'lqq', :email => 'lqq@gmail.com')
