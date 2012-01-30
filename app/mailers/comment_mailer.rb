30dcc594555073876a2cf477708e42e1f421c575
Jingjing Duan <duanjingjing@gmail.com>
2012-01-29 20:35:21 -0800

    make delayed_mail work

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,8 +1,9 @@
 class CommentMailer < ActionMailer::Base
-  default from: "puzzlenodetest@gmail.com"
+  default :from => "puzzlenodetest@gmail.com"
 
   def comment_made(subject, to, body)
-    @body = body
-    mail(:to => to, :subject => subject)
+    mail(:to => to, :subject => subject) do |format|
+      format.text {render :text => body}
+    end
   end
 end
