4cb1cef0298afd4048ed40bbddbf2fc30d70f595
Jingjing Duan <duanjingjing@gmail.com>
2012-01-29 21:11:28 -0800

    move smtp settings to an initializer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,19 +13,6 @@ Puzzlenode::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :mailhopper
-  config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :authentication       => 'plain',
-    :user_name            => 'puzzlenodetest@gmail.com',
-    :password             => 'mendicant-s10',
-  }
-
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
