19c35219e136c63ccf450c3c1e713c291b62f671
Jingjing Duan <duanjingjing@gmail.com>
2012-01-30 19:00:04 -0800

    add back ActionMailer config to development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,5 +24,18 @@ Puzzlenode::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :mailhopper
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :authentication       => 'plain',
+    :user_name            => 'puzzlenodetest@gmail.com',
+    :password             => 'mendicant-s10',
+  }
 end
 
