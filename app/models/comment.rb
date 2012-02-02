6088adc828c6301b08747ef32e20961ed1c9eb6e
Jordan Byron <jordan.byron@gmail.com>
2012-02-01 22:22:40 -0500

    Administrators should be able to get comment emails too

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,8 +7,11 @@ class Comment < ActiveRecord::Base
   # Notify the other users who have solved this puzzle
   # when a new comment is made.
   def notify_comment_made
-    users  = puzzle.solved_by.where(:notify_comment_made => true) - [user]
-    emails = users.map(&:email).compact
+    users   = puzzle.solved_by.where(:notify_comment_made => true)
+    users  += User.where(:admin => true, :notify_comment_made => true)
+    users  -= [ user ]
+
+    emails  = users.map(&:email).compact.uniq
 
     emails.each_slice(25) do |e|
       CommentMailer.comment_made(self, e).deliver
