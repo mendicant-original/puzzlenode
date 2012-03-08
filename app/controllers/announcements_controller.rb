199cf8cfa645a36993dba6e02371bb2e53576de9
Jordan Byron <jordan.byron@gmail.com>
2012-03-08 10:32:50 -0500

    Damn you google crawlers! Spaming my inbox

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,16 +1,20 @@
 class AnnouncementsController < ApplicationController
+  respond_to :html
+
   def index
     @announcements = Announcement.order("created_at DESC")
-    
-    respond_to do |format|
+
+    respond_with(@announcements) do |format|
       format.html do
         @announcements = @announcements.paginate(:page => params[:page], :per_page => 10)
       end
       format.rss { render :layout => false }
     end
   end
-  
+
   def show
     @announcement = Announcement.find(params[:id])
+
+    respond_with(@announcement)
   end
 end
