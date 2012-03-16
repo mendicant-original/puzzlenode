0e086976c1b02446f6d5c1faa71acc8674d071e9
Jordan Byron <jordan.byron@gmail.com>
2012-03-16 09:26:31 -0600

    Enable autolink and use MdPreview for markdown parsing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,13 +5,7 @@ module ApplicationHelper
   end
 
   def md(content)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-      :autolink            => true,
-      :space_after_headers => true,
-      :no_intra_emphasis   => true,
-      :fenced_code_blocks  => true)
-
-    markdown.render(content).html_safe
+    MdPreview::CustomParser.parse(content)
   end
 
 end
--- config/initializers/markdown.rb
+++ config/initializers/markdown.rb
@@ -4,6 +4,7 @@ module MdPreview
 
     def parse(content)
       markdown = Redcarpet::Markdown.new(MdEmoji::Render,
+              :autolink            => true,
               :space_after_headers => true,
               :no_intra_emphasis   => true,
               :fenced_code_blocks  => true)
