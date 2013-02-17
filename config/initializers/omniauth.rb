d835dc4848531e93c4bd3fe7a231b66fcaa8a06f
Johnny Mukai <johnny.mukai@gmail.com>
2013-02-16 19:44:31 -0500

    Use consistent style for env vars

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer, fields: [:name, :email, :nickname] unless Rails.env.production?
-  provider :github, ENV['github_consumer_key'], ENV['github_consumer_secret']
+  provider :github, ENV['GITHUB_CONSUMER_KEY'], ENV['GITHUB_CONSUMER_SECRET']
 end
