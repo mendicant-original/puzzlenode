afbb572fe45917bf70031cbbecf402ad54a5385e
Jordan Byron <jordan.byron@gmail.com>
2014-07-22 11:07:22 -0400

    Ignore the infamous EasouSpider crawler

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -2,5 +2,6 @@ if Rails.env.production?
   Puzzlenode::Application.config.middleware.use ExceptionNotifier,
     :email_prefix => "[Puzzlenode] ",
     :sender_address => %{"Puzzlenode Notifier" <notifier@puzzlenode.com>},
-    :exception_recipients => %w{jordan.byron@gmail.com puzzlenode@gmail.com}
+    :exception_recipients => %w{jordan.byron@gmail.com puzzlenode@gmail.com},
+    :ignore_crawlers      => %w{EasouSpider}
 end
